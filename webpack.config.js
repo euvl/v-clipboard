44ec204aed710f1fad3cd4c9dda84186468a7312
euvl <yev.vlasenko@gmail.com>
2017-07-06 06:34:56 +0100

    Added configuration & dist files for v1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'index.min.js',
+    library:'v-clipboard',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+//      compress: false,
+//      comments: false,
+      sourceMap: true
+    })
+  ]
+}
