184e3cf9790efcbeaee43da72fb9841739dad640
euvl <yev.vlasenko@gmail.com>
2017-07-06 06:44:59 +0100

    Minor refactoring

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@
   * Copyright (c) 2017 - Yev Vlasenko
   */
 const copy = (text) => {
-  var textArea = document.createElement("textarea")
+  let textArea = document.createElement("textarea")
+  let success = false
 
+  textArea.value = text
   textArea.style.cssText = 
     'position:fixed;pointer-events:none;visible:hidden;'
-  textArea.value = text
     
   document.body.appendChild(textArea)
   textArea.select()
-
-  let success = false
 
   try {
     success = document.execCommand('copy')
@@ -31,7 +30,7 @@ export default {
       bind (el, binding, vnode) {
         el.addEventListener('click', (event) => {
           if (binding.hasOwnProperty('value')) {
-            let value = binding.value
+            let { value } = binding
             
             copy(value)
               ? vnode.$emit('copy', value)
