fdfbbb3d9ba37f3cb971b8166efda8b8e40ec5fe
euvl <yev.vlasenko@gmail.com>
2017-07-06 10:16:28 +0100

    Fixed buggy css that was not working in all browsers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-/** 
+/**
   * Copyright (c) 2017 - Yev Vlasenko
   */
-const cssText = 
-  'position:fixed;pointer-events:none;z-index:-9999;visibility:hidden;'
+const cssText =
+  'position:fixed;pointer-events:none;z-index:-9999;opacity:0;'
 
 const copy = (text) => {
   let textArea = document.createElement("textarea")
@@ -10,7 +10,7 @@ const copy = (text) => {
 
   textArea.value = text
   textArea.style.cssText = cssText
-    
+
   document.body.appendChild(textArea)
   textArea.select()
 
@@ -35,7 +35,7 @@ export default {
             let { value } = binding
             let payload = { value, srcEvent: event }
             let context = vnode.context
-            
+
             copy(value)
               ? context.$emit('copy', payload)
               : context.$emit('copyError', payload)
