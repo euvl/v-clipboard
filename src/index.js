a87d9fbe50effcffbad6af8577e170a6206b7419
Yev Vlasenko <yev.vlasenko@gmail.com>
2018-05-31 12:38:55 +0300

    Rewrote plugin

--- src/index.js
+++ src/index.js
@@ -1,55 +1,118 @@
 /**
-  * Copyright (c) 2017 - Yev Vlasenko
-  */
-const cssText =
-  'position:fixed;pointer-events:none;z-index:-9999;opacity:0;'
+ * Copyright (c) 2017 - 2018 - Yev Vlasenko
+ */
+const cssText = "position:fixed;pointer-events:none;z-index:-9999;opacity:0;";
+const copyErrorMessage = "Failed to copy value to clipboard. Unknown type.";
 
-const copy = (text) => {
-  if (typeof text !== 'string') {
+const $clipboard = text => {
+  if (typeof text !== "string") {
     try {
-      text = JSON.stringify(text)
+      text = JSON.stringify(text);
     } catch (e) {
-      throw 'Failed to copy value to clipboard. Unknown type.'
+      throw copyErrorMessage;
     }
   }
- 
-  let textArea = document.createElement("textarea")
-  let success = false
 
-  textArea.value = text
-  textArea.style.cssText = cssText
+  const textArea = document.createElement("textarea");
+  let success = false;
 
-  document.body.appendChild(textArea)
-  textArea.select()
+  textArea.value = text;
+  textArea.style.cssText = cssText;
+
+  document.body.appendChild(textArea);
+  textArea.select();
 
   try {
-    success = document.execCommand('copy')
+    success = document.execCommand("copy");
   } catch (err) {
-    // success = false
+    console.warn(err);
   }
 
-  document.body.removeChild(textArea)
-  return success
-}
+  document.body.removeChild(textArea);
+
+  return success;
+};
 
 export default {
-  install (Vue) {
-    Vue.prototype.$clipboard = copy
+  install(Vue) {
+    Vue.prototype.$clipboard = $clipboard;
 
-    Vue.directive('clipboard', {
-      bind (el, binding, vnode) {
-        el.addEventListener('click', (event) => {
-          if (binding.hasOwnProperty('value')) {
-            let { value } = binding
-            let payload = { value, srcEvent: event }
-            let context = vnode.context
+    const generateId = (id => () => "$" + id++)(1);
+    const handlers = {};
 
-            copy(value)
-              ? context.$emit('copy', payload)
-              : context.$emit('copyError', payload)
-          }
-        })
+    const removeHandler = id => {
+      if (id) {
+        handlers[id] = null;
+        delete handlers[id];
       }
-    })
+    };
+
+    const addHandler = func => {
+      const id = generateId();
+      handlers[id] = func;
+
+      return id;
+    };
+
+    Vue.directive("clipboard", {
+      bind(el, binding, vnode) {
+        const { arg, value } = binding;
+
+        switch (arg) {
+          case "error":
+            const errorHandlerId = addHandler(value);
+            el.dataset.clipboardErrorHandler = errorHandlerId;
+            return;
+
+          case "success":
+            const successHandlerId = addHandler(value);
+            el.dataset.clipboardSuccessHandler = successHandlerId;
+            return;
+
+          default:
+            const clickEventHandler = event => {
+              if (binding.hasOwnProperty("value")) {
+                const payload = {
+                  value: typeof value === "function" ? value() : value,
+                  event
+                };
+
+                const handlerId = $clipboard(payload.value)
+                  ? el.dataset.clipboardSuccessHandler
+                  : el.dataset.clipboardErrorHandler;
+
+                const handler = handlers[handlerId];
+
+                if (handler) {
+                  handler(payload);
+                }
+              }
+            };
+
+            const clickEventHandlerId = addHandler(clickEventHandler);
+
+            el.dataset.clipboardClickHandler = clickEventHandlerId;
+            el.addEventListener("click", handlers[clickEventHandlerId]);
+
+            return;
+        }
+      },
+
+      unbind(el, binding, vnode) {
+        const {
+          clipboardSuccessHandler,
+          clipboardErrorHandler,
+          clipboardClickHandler
+        } = el.dataset;
+
+        removeHandler(clipboardSuccessHandler);
+        removeHandler(clipboardErrorHandler);
+
+        if (clipboardClickHandler) {
+          el.removeEventListener("click", handlers[clipboardClickHandler]);
+          removeHandler(clipboardClickHandler);
+        }
+      }
+    });
   }
-}
+};
